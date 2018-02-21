70b42a1853b404092abe7f7f33b028f43e22fc63
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-02-21 17:09:15 +0200

    refactored

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,43 @@
 $('#for-test-delete-later').on("click", function () {
     $.getJSON("../data/data.json", function (data) {
-        let loc = data.wallpapers[0].location;
-        let $img = $('<img>');
-        $img.addClass("testCatPic");
-        $img.attr('src', loc);
-        $img.appendTo('#main-content');
-
-        let $div = $('<div>');
-        $div.addClass("media");
-
-        for (let i = 0; i < data.wallpapers[0].comments.length; i++) {
-            const comment = data.wallpapers[0].comments[i];
-            const author = comment.author;
-            const content = comment.content;
-
-            let $divMC = $('<div>');
-            //$divMC.addClass("media-body");
-
+        for (let i = 0; i < data.categories[0].animals.length; i++) {
+            let loc = data.categories[0].animals[i].location;
             let $img = $('<img>');
-            $img.addClass("img-thumbnail");
-            $img.attr('src', "../imgs/logoPW.png");
+            $img.addClass("testCatPic");
+            $img.attr('src', loc);
+            $img.appendTo('#main-content');
 
-            let $h5 = $('<h5>');
-            $h5.addClass("author");
-            $h5.text(author);
+            let $div = $("<div>");
+            $div.addClass("media");
 
-            let $p = $('<p>');
-            $p.text(content);
+            for (let j = 0; j < data.categories[0].animals[i].comments.length; j++) {
+                const comment = data.categories[0].animals[i].comments[j];
+                const author = comment.author;
+                const content = comment.content;
 
-            $divMC.append($h5);
-            $divMC.append($img);
-            $divMC.append($p);
+                let $divMC = $('<div>');
+                //$divMC.addClass("media-body");
 
-            $div.append($divMC);
+                let $img = $('<img>');
+                $img.addClass("img-thumbnail");
+                $img.attr('src', "../imgs/logoPW.png");
+
+                let $h5 = $("<h5>");
+                $h5.addClass("author");
+                $h5.text(author);
+
+                let $p = $('<p>');
+                $p.text(content);
+
+                $divMC.append($h5);
+                $divMC.append($img);
+                $divMC.append($p);
+
+                $div.append($divMC);
+            }
+            $div.appendTo('#main-content');
         }
 
-        $div.appendTo('#main-content');
     });
+
 })
