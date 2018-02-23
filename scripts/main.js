8d8a05bc55a1717c7ee40a0fc44c2bc19bcddb30
cheficha <stefanzhekov95@gmail.com>
2018-02-23 15:54:17 +0200

    ctyjb

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,8 @@
         $(configuration.main).show("drop", {}, 1000);
     });
 
-<<<<<<< HEAD
     $("#our-team").on("click", function(){
        $(configuration.main).html(htmlLoader.loadAboutUs());
     })
 })();
 
-=======
-})();
->>>>>>> 265f8242d060a0cc07a790c2cabb871a7f77446e
