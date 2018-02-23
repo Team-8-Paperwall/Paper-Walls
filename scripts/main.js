aa174d38174bea12fcf079606d4bad1890c08282
Iskra Lumbeva <iskra.lumbeva@gmail.com>
2018-02-23 15:38:02 +0200

    About us page created.

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,9 @@
     database.getAll().then((all) => {
         htmlLoader.loadGrid(all);
     });
+
+    $("#our-team").on("click", function(){
+       $(configuration.main).html(htmlLoader.loadAboutUs());
+    })
 })();
+
