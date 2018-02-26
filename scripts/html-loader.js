1691ef717c7bb3af691903d2393362585079543a
cheficha <stefanzhekov95@gmail.com>
2018-02-26 10:42:13 +0200

    merge

--- scripts/html-loader.js
+++ scripts/html-loader.js
@@ -2,27 +2,7 @@
 
 const htmlLoader = (function () {
     const loadGrid = function (wallpapers) {
-<<<<<<< HEAD
-        let $gridContainer = $("<div>");
-        $gridContainer.addClass("grid-container");
-
-        for (let i = 0; i < wallpapers.length; i++) {
-            let wallpaper = wallpapers[i];
-
-            let $img = $("<img>");
-            $img.attr("src", wallpaper.location);
-            $img.addClass("grid-wallpaper");
-            let $imgContainer = $("<div>");
-            $imgContainer.html($img);
-            $imgContainer.addClass("grid-wallpaper-container");
-
-            $gridContainer.append($imgContainer);
-        }
-
-        return $gridContainer;
-=======
         return gridTemplate(wallpapers);
->>>>>>> f7247f35f3259e104742868b34e3e5c3069db368
     };
 
     const loadAboutUs = function () {
