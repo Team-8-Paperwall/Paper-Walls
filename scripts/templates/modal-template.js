c89d75d9bbc33fb5a1ec0e6d7c332c7576d9671f
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-02-28 22:45:10 +0200

    Changed download button color

--- scripts/templates/modal-template.js
+++ scripts/templates/modal-template.js
@@ -4,7 +4,7 @@ const modalTemplate = function () {
   <div id="myModal" class="modal">
         <span class="close">&times;</span>
         <img class="modal-content" id="img01">
-        <button id="downloadButton" type="button" class="btn btn-primary">Download</button>  
+        <button id="downloadButton" type="button" class="btn btn-success">Download</button>  
         <a id="tempLink" href="" download="paperWallsWallpaper"></a>      
         <div id="caption"></div>
     </div>    
